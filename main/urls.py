c6c3d1c98c56f04deb391f05f29bcbfac4784739
qkrqod21 <hgang4325@gmail.com>
2026-01-28 14:23:51 +0900

    신청하기, 시뮬레이션 버튼 추가

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("api/getPolicyData", views.getPolicyData, name="getPolicyData")
+    path("api/getPolicyData", views.getPolicyData, name="getPolicyData"),
+    
+    path("policy/apply/", views.apply_steps, name="apply_steps"),
+    
+    path("policy/simulate/", views.simulate, name="simulate"),
 ]
