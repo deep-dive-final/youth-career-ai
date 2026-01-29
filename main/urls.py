2d786c9b845fb460e44c6cc75ced9aea1f275f44
qkrqod21 <hgang4325@gmail.com>
2026-01-29 13:40:39 +0900

    경로 추가

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("api/getPolicyData", views.getPolicyData, name="getPolicyData"),
     
-    path("policy/apply/", views.apply_steps, name="apply_steps"),
+    path("policy/apply/", views.apply_steps, name="apply_steps"), # 신청하기 페이지(서류 작성으로 연결)
     
-    path("policy/simulate/", views.simulate, name="simulate"),
+    path("policy/simulate/", views.simulate, name="simulate"), # 시뮬레이션 페이지
+
+    path("policy/apply/form/", views.apply_form, name="apply_form"), # AI 신청서 페이지
+
+    path('policy/detail/', views.policy_detail, name='policy_detail'), #상세페이지
 ]
