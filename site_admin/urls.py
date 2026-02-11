129c20082d32a841d7f14861f684bf64206b1244
seyeon-night <seyeon-night@gmail.com>
2026-02-11 11:46:04 +0900

    정책 정보 수정 화면

--- site_admin/urls.py
+++ site_admin/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path("data", views.data, name="data"),
+    path("list", views.data_list, name="data_list"),
     path("api/importData", views.importData, name="importData"),
     path("api/getSearchData", views.getSearchData, name="getSearchData"),
     path("api/uploadFile", views.upload_file, name="uploadFile"),
+    path("api/getKeywordData", views.get_keyword_data, name="getKeywordData"),
+    path("api/setKeywordData", views.set_keyword_data, name="setKeywordData"),
 ]
