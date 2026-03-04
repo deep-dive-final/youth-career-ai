5dd0bd5ccbf284e8855b8cb1869279eff2982744
seyeon-night <seyeon-night@gmail.com>
2026-03-04 18:03:06 +0900

    urls.py 수정

--- site_admin/urls.py
+++ site_admin/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("data", views.data, name="data"),
     path("list", views.data_list, name="data_list"),
     path("api/importData", views.importData, name="importData"),
